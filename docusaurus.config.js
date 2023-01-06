1c3b04352c7b06cd33379413e10191b99d915d9f
bridgerhung <48978583+bridgerhung@users.noreply.github.com>
2023-01-06 21:59:28 +0800

    1 file modified

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,12 @@ const config = {
         // Remove this to remove the "edit this page" links.
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
-        },
+          [
+            require.resolve("@easyops-cn/docusaurus-search-local"),
+            {
+              hashed: true,
+            },
+          ],
       }),
     ],
   ],
