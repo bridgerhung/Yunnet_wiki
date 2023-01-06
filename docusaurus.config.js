e25de8c748a274cde06d57235380c3f70f610054
bridgerhung <48978583+bridgerhung@users.noreply.github.com>
2023-01-06 22:56:04 +0800

    1 file modified, 1 deleted

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,72 +1,90 @@
-const buildType = process.env.BUILD_TYPE;
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
 
-module.exports = {
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+/** @type {import("@easyops-cn/docusaurus-search-local") */
+const config = {
   title: '雲科網管wiki',
   tagline: '這裡專放一些網管的相關資訊',
   url: 'https://yunnet.brid.gq',
   baseUrl: '/',
   onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
   favicon: 'img/Yunnet_icon.svg',
-  organizationName: 'bridgerhung',
-  projectName: 'Yunnet_wiki',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'bridgerhung', // Usually your GitHub org/user name.
+  projectName: 'Yunnet_wiki', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'zh-Hant',
     locales: ['zh-Hant'],
   },
 
-  themeConfig: {
-    navbar: {
-      title: 'Yunnet wiki',
-      logo: {
-        alt: 'Yunnet logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'doc',
-          docId: '/category/社團課程資料',
-          position: 'left',
-          label: '社團課程資料',
-        },
-        {
-          type: 'doc',
-          docId: '/category/宿舍網路',
-          position: 'left',
-          label: '宿舍網路',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} 雲科網管`,
-    },
-  },
   presets: [
     [
-      '@docusaurus/preset-classic',
-      {
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      /** @type {import("@easyops-cn/docusaurus-search-local") */
+      ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
         },
         blog: false,
-
+        // Please change this to your repo.
+        // Remove this to remove the "edit this page" links.
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      },
-    ],
-  ],
-  themes: [
-    [
-      '@easyops-cn/docusaurus-search-local',
-      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
-      ({
-        hashed: true,
-        language: ['en', 'zh'],
-        highlightSearchTermsOnTargetPage: true,
-        explicitSearchResultPath: true,
       }),
     ],
   ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+    ({
+      navbar: {
+        title: `Yunnet wiki `,
+        logo: {
+          alt: 'Yunnet logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: '/category/社團課程資料',
+            position: 'left',
+            label: '社團課程資料',
+          },
+          {
+            type: 'doc',
+            docId: '/category/宿舍網路',
+            position: 'left',
+            label: '宿舍網路',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+
+        copyright: `Copyright © ${new Date().getFullYear()} 雲科網管`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
+
+module.exports = config;
