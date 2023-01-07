6ad0fff0590279f4f5c343106bc90446880b9da6
bridgerhung <48978583+bridgerhung@users.noreply.github.com>
2023-01-07 20:46:50 +0800

    1 file modified

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,6 +36,9 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
+        older: {
+          routeBasePath: '/category/過去學年度封存',
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
         },
