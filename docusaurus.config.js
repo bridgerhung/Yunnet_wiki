219aadb69ab4f53f9646f3495cd3a3827321a5ef
bridgerhung <48978583+bridgerhung@users.noreply.github.com>
2023-01-07 16:42:13 +0800

    2 files modified

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          lastVersion: 'current',
+          lastVersion: '111',
 
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
