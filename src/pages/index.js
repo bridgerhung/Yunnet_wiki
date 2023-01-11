1f5750d90cb78b465b0580cbc43f890ec5f06c62
bridgerhung <48978583+bridgerhung@users.noreply.github.com>
2023-01-11 12:34:47 +0800

    1 file modified

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
   return (
     <Layout
       title={` ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description="網管維基"
     >
       <HomepageHeader />
       <main>
