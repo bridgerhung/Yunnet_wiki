92c6f7be3df8d200c6eabd3545232dd6ef3b561f
bridger <48978583+bridgerhung@users.noreply.github.com>
2023-05-08 06:40:24 +0000

    Update 2 files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -122,9 +122,9 @@ const config = {
         darkTheme: darkCodeTheme,
       },
       announcementBar: {
-        id: "announcementBar-11", // Increment on change
-        content: '<b><a href="/111/下學期/2023-05-03%20tmux">5/4 Tmux </a></b>',
-        //content: '<b><a href="/111/Sunday/2023-04-30%20GCP%20BigQuery">4/30 研討會</a></b>'
+        id: "announcementBar-12", // Increment on change
+        //content: '<b><a href="/111/下學期/2023-05-03%20tmux">5/4 Tmux </a></b>',
+        content: '<b><a href="/111/Sunday/2023-05-07%20SSH_SCP">5/8 研討會</a></b>',
         isCloseable: true,
       },
       algolia: {
