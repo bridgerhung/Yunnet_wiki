8b47d47b090a2981e1d766d092f556ecedcc0191
bridger <48978583+bridgerhung@users.noreply.github.com>
2024-04-04 06:36:48 +0000

    餅乾開會

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -122,7 +122,7 @@ const config = {
         id: "announcementBar-12", // Increment on change
         //content: '<b><a href="/111/Sunday/2023-05-28%20Cisco_Switch">5/28 交換器 </a></b>',
         content:
-          '<b><a href="112/112-2/2024-03-06%20JS">03-06JavaScript</a></b>',
+          '<b><a href="112/112-2/2024-03-27%20cookie%20and%20session">餅乾開會</a></b>',
         isCloseable: true,
       },
       algolia: {
